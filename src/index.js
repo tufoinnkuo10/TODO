528de754ab34d16c90b127b8d5d58823a2272e98
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-09-29 00:15:10 +0100

    create structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import _ from 'lodash';
+
 import './style.css'
 
-function component() {
-  const element = document.createElement('div');
-  element.classList.add('newclass')
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const e
