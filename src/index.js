6e4bfe2687293e397635b239635fde880369215c
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-09-30 17:44:27 +0100

    local storage

--- src/index.js
+++ src/index.js
@@ -1,42 +1,63 @@
 import './style.css';
-import displayBox from './checkbox.js';
+import { taskCompleted } from './checkbox.js'
+// import { taskCompleted, items } from './checkbox.js';
 
-const myTasks = document.querySelector('.task-list');
+const itemsContainer = document.querySelector('.items-container');
+const input = document.createElement('input');
+const inputCont = document.querySelector('.input');
+const title = document.querySelector('.title');
+const titleText = document.createElement('p');
+const deleteCont = document.querySelector('.delete');
+const deleteText = document.createElement('p');
+const icon = document.createElement('i');
+const enter = document.createElement('i');
 
-const taskArray = [
-  {
-    description: 'sports',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'code',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'sleep',
-    completed: false,
-    index: 2,
-  },
-];
+input.type = 'text';
+input.setAttribute('placeholder', 'Enter a new task');
+enter.classList.add('fas', 'fa-level-down-alt', 'rotate');
+inputCont.appendChild(input);
+inputCont.appendChild(enter);
 
-const showTasks = (tasks) => `
-<li data-id="${tasks.index}">
-<input type="checkbox" name="" class="static-list" data-id="${tasks.index}" class = "check-list">
-<label class="list" for="">${tasks.description}<i class="fas fa-ellipsis-v"></i></label>
-</li>
-`;
-myTasks.innerHTML = taskArray.map((tasks) => showTasks(tasks)).join('');
+titleText.textContent = 'Todays todo';
+icon.classList.add('fas', 'fa-sync');
+title.appendChild(titleText);
+title.appendChild(icon);
 
-displayBox();
+deleteText.textContent = 'Clear all completed';
+deleteCont.appendChild(deleteText);
 
-// add local Storage
+let items = [];
+let indexCont = 0;
 
-const taskArraySerialised = JSON.stringify(taskArray);
+if (localStorage.getItem('items')) {
+  items = [...JSON.parse(localStorage.getItem('items'))];
+  items.forEach((item) => {
+    const div = document.createElement('div');
+    div.classList.add('task');
+    div.id = `${indexCont += 1}`;
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = item.completed;
+    checkbox.addEventListener('change', (e) => taskCompleted(e, items));
+    const p = document.createElement('p');
+    p.textContent = item.description;
+    const icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-ellipsis-v', 'flex-end');
+    div.appendChild(checkbox);
+    div.appendChild(p);
+    div.appendChild(icon);
+    itemsContainer.appendChild(div);
+    if (item.completed) {
+      div.classList.add('completed');
+    }
+  });
+}
+class Item {
+  constructor() {
+    this.description = '';
+    this.completed = false;
+    this.id = '';
+  }
+}
 
-localStorage.setItem('taskArray', taskArraySerialised);
 
-const taskArrayDeserialised = JSON.parse(localStorage.getItem('taskArray'));
-
-taskArrayDeserialised();
