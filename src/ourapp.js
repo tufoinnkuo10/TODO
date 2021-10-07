97fbf798ea059abf4ce1aad6aedb7ab14440b7db
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-07 15:15:12 +0100

    add remove test changes

--- src/ourapp.js
+++ src/ourapp.js
@@ -1,2 +1,12 @@
 //  FUNCTIONS FOR ADD AND REMOVE TODO
 
+export const removeTodos = (something, id) => {
+  if (something.index.toString() === id) {
+    return false;
+  }
+  return true;
+};
+
+export const addTodo = (itemsArray, value) => {
+  itemsArray.push({ completed: false, description: value, index: itemsArray.length + 1 });
+};
